da97c48733821cacc3df04bd019594760bdced36
Ivan Valenkov <ivanvalenkov@gmail.com>
2019-05-27 19:46:59 +0300

    Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,262 @@
-from django.db import models
+# from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.utils.timezone import now
+from django.core.cache import cache
+from django.dispatch import receiver
+from django.db.models.signals import post_save, pre_save
+from django.core.exceptions import ValidationError
+from django.db.models import \
+    CharField, UUIDField, Model, IntegerField, DateTimeField, \
+    BooleanField, ManyToManyField, TextField, EmailField, \
+    ForeignKey, FloatField, OneToOneRel, PROTECT, OneToOneField, ManyToOneRel
 
-# Create your models here.
+from django.utils.translation import ugettext_lazy as _
+import uuid
+from django.db.models import Sum
+from django.db.models import Q
+
+from .validators import inn_validator
+
+from django.db import transaction
+
+
+class BaseModel(Model):
+    class Meta:
+        abstract = True
+
+    id = UUIDField(db_column='Id', primary_key=True, default=uuid.uuid4)
+
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+
+class EUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not date_of_birth:
+            date_of_birth = now()
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        if not email:
+            email = 'ivanvalenkov@gmail.com'
+        if not date_of_birth:
+            date_of_birth = now()
+
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class CashTransaciton(BaseModel):
+    class Meta:
+        verbose_name = _("currency transfers")
+        verbose_name_plural = _("currency transfers")
+
+    def pay_validator(self, val):
+        if val < 0:
+            raise ValidationError(_(f"Credit  {val}<0 "))
+        if self.src.balance < self.val:
+            raise ValidationError(_(f"Not enough user ({self.src.get_full_name()}) money"))
+
+    tid = UUIDField(primary_key=False, default=uuid.uuid4,
+                    unique=False,  # may be many recipients
+                    null=False)
+
+    val = FloatField(help_text="currency", validators=[pay_validator],
+                     verbose_name="Amount transfer", )
+    src = OneToOneField("EUser", related_name="src_user_id", to_field="id", on_delete=PROTECT, null=True)
+    dst = OneToOneField("EUser", related_name="dst_user_id", to_field="id", on_delete=PROTECT, null=True)
+    active = BooleanField(verbose_name="Active", help_text="Transaction accepted", default=True)
+
+    def __str__(self):
+        return f"{self.tid} {self.src} to {self.dst} {self.val} {self.active}"
+
+    def activate(self) -> bool:
+        raise NotImplementedError("TODO")
+
+    def deactivate(self) -> bool:
+        # Распроведение одиночной транзакции
+        raise NotImplementedError("TODO")
+
+    def deactivate_cascade(self):
+        # Каскадное распроведение переводов
+        raise NotImplementedError("TODO")
+
+
+class EUser(AbstractUser, BaseModel):
+    middle_name = CharField(_('Отчество'), max_length=30, blank=True, null=True)
+
+    inn = CharField(_("INN"), unique=False, blank=False, null=False, validators=[inn_validator], max_length=12)
+
+    # кешированное поле
+    cached_balance = FloatField(_('cached balance'), null=True, blank=True, unique=False)
+
+    @property
+    def transactions(self):
+        return CashTransaciton.objects.filter(Q(src=self) | Q(dst=self)).all()
+
+    # REQUIRED_FIELDS = []
+    class Meta:
+        # db_table = "User"
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    @property
+    def balance(self):
+        c = cache.get(self.id)
+        if c:
+            return c
+        else:
+            balance = self.balance_rebuild()
+            self.cached_balance = balance
+            self.save()
+            return balance
+
+    def get_credit_transactions(self):
+        return CashTransaciton.objects.filter(dst=self, active=True)
+
+    def get_credit(self) -> float:
+        return self.get_credit_transactions().aggregate(Sum('val'))
+
+    def get_debt_transactions(self):
+        return CashTransaciton.objects.filter(src=self, active=True)
+
+    def get_debt(self) -> float:
+        return self.get_debt_transactions().aggregate(Sum('val'))
+
+    def balance_rebuild(self) -> float:
+        ret = self.get_credit() - self.get_debt()
+        self.cached_balance = ret
+        self.save()
+        return ret
+
+    @staticmethod
+    def balance_rebuild_for_all():
+        # TODO: Временное решение для тестирования. Это нужно оптимизировать, например sql процедурой.
+        for u in EUser.objects.all():
+            u.balance_rebuild()
+
+    def make_receipt(self, val: float):
+        """
+        сделать поступление
+        :param val:
+        :return:
+        """
+        t = CashTransaciton(val=val, src=None, dst=self)
+        t.save()
+
+    def make_expense(self, val: float) -> bool:
+        """
+        сделать списание
+        :param val:
+        :return:
+        """
+        # expended val checks in validator
+        t = CashTransaciton(val=val, src=self, dst=None)
+        try:
+            t.save()
+            return True
+        except ValueError:
+            return False
+
+    @transaction.atomic
+    def make_transfer(self, val, user=None, inn: str = None) -> bool:
+        """
+        Перевод для пользователя, или для нескольких по инн
+        :param val:
+        :param user:
+        :param inn:
+        :return:
+        """
+        if user is not None and inn is not None:
+            raise AssertionError("Недопустимая ситуация, когда при переводе указан пользователь и инн")
+        else:
+            if user:
+                try:
+                    CashTransaciton(val=val, src=self, dst=user).save()
+                    return True
+                except:
+                    return False
+            else:
+                users = EUser.objects.filter(inn=inn)
+                cnt = len(users)
+                part = round(val / cnt, 2)
+                sid = transaction.savepoint()
+                for u in users:
+                    if not u.make_transfer(part, user=u):
+                        transaction.savepoint_rollback(sid)
+                        return False
+                transaction.savepoint_commit(sid)
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = f"{self.last_name} {self.first_name} {self.middle_name}"
+        return full_name.strip()
+
+    @staticmethod
+    def _split_name(s='', i=0):
+        # Фадеева         Лариса         Константиновна
+        s = s.replace('  ', ' ')
+        spl = s.split(' ')
+        try:
+            ret = spl[i]
+        except Exception:
+            ret = None
+        return ret
+
+    @classmethod
+    def extract_first_name(cls, s=''):
+        return cls._split_name(s, 1)
+
+    @classmethod
+    def extract_last_name(cls, s=''):
+        return cls._split_name(s, 0)
+
+    @classmethod
+    def extract_middle_name(cls, s=''):
+        return cls._split_name(s, 2)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.middle_name}"
+
+
+@receiver(pre_save, sender=CashTransaciton)
+def _on_cash_transaction_save(sender, instance: CashTransaciton, **kwargs):
+    instance.updated_at = now()
+
+
+@receiver(pre_save, sender=EUser)
+def _on_cash_transaction_save(sender, instance: CashTransaciton, **kwargs):
+    instance.updated_at = now()
+
+
+@receiver(post_save, sender=CashTransaciton)
+def _on_cash_transaction_save(sender, instance: CashTransaciton, **kwargs):
+    if instance.active:
+        cache.delete(instance.src.id)
+        cache.delete(instance.dst.id)
--- benovate/settings.py
+++ benovate/settings.py
@@ -10,23 +10,38 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import os, platform
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PRODUCTION_PLATFORM_NODE = 'prodserver'
+
+# SECURITY WARNING: keep the secret key used in production secret!
+if platform.node() != PRODUCTION_PLATFORM_NODE:
+    SECRET_KEY = '9mml1zfw(yxtqu)0kgge#^290$wzh)j0zw9%g1md8oa=bld3p7'
+else:
+    try:
+        from .secret import SECRET_KEY
+    except:
+        raise Exception("Нужно определить секретный ключ secret.py ")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't33#kw^-jhd-hx=9hjgl=*3)ewgmkwl#7pzk5@)r2$h!)7=#sd'
+# SECRET_KEY = 't33#kw^-jhd-hx=9hjgl=*3)ewgmkwl#7pzk5@)r2$h!)7=#sd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = platform.node() != PRODUCTION_PLATFORM_NODE
 
 ALLOWED_HOSTS = ['*']
 
+LOGOUT_REDIRECT_URL = '/'
+
+
+AUTH_USER_MODEL = 'app.EUser'
 
 # Application definition
 
@@ -38,7 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
-    'rest_framework'
+    'rest_framework',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'benovate.urls'
@@ -72,7 +89,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'benovate.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -82,7 +98,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -102,7 +117,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -116,8 +130,42 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+JSON_API_FORMAT_FIELD_NAMES = 'camelize'
+JSON_API_FORMAT_TYPES = 'camelize'
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 5,
+    'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework_json_api.pagination.JsonApiPageNumberPagination',
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework_json_api.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework_json_api.renderers.JSONRenderer',
+
+        # If you're performance testing, you will want to use the browseable API
+        # without forms, as the forms can generate their own queries.
+        # If performance testing, enable:
+        # 'example.utils.BrowsableAPIRendererWithoutForms',
+        # Otherwise, to play around with the browseable API, enable:
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework_json_api.filters.OrderingFilter',
+        'rest_framework_json_api.django_filters.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ),
+    'SEARCH_PARAM': 'filter[search]',
+    'TEST_REQUEST_RENDERER_CLASSES': (
+        'rest_framework_json_api.renderers.JSONRenderer',
+    ),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json'
+}
